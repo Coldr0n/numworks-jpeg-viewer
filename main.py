43727a02d35329c8d93b00485e121fff8701bc3e
Coldr0n <120742272+Coldr0n@users.noreply.github.com>
2025-01-29 17:58:30 +0100

    Added a simple ByteReader class

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from PIL import Image
 from io import BytesIO
+from typing import Literal
 
 NUMWORKS_SIZE = 320, 222
 
 def create_webp_pyfile(filepath: str, output_file_name: str) -> bytes:
     """
-    This function takes an image path and scale it to the numworks viewport size. It then saves the data as a byte string into a python file
-    Returns the bytes saved
+    This function takes an image path and scale it to the numworks viewport size.
+    It then convert and compress the image into a webp file and write the image's byte into a python file.
+    Returns the bytes saved.
     """
     with Image.open(filepath) as img:
         img = img.crop(img.getbbox()) # Crop the image to the actual bounding box
@@ -28,7 +30,36 @@ def create_webp_pyfile(filepath: str, output_file_name: str) -> bytes:
         with open(output_file_name, "w") as out_file:
             out_file.write("buffer = " + str(output.getvalue()))
 
+class ByteReader:
+    def __init__(self, buffer: bytes, as_big_endian: bool = True):
+        self.buffer = buffer
+        self._pos = 0
+        self._endianness = as_big_endian # True for big endian
+
+    def read(self, nbytes: int, as_big_endian: bool | None = None) -> bytes:
+        """
+        Read a block of data from the buffer and returns it
+        """
+        data = self.buffer[self._pos : self._pos + nbytes]
+        self._pos += nbytes
+
+        if as_big_endian is None:
+            as_big_endian = self._endianness
+
+        return data if as_big_endian else data[::-1]
+    
+    def skip(self, nbytes: int) -> None:
+        self._pos += nbytes
+
+def decode_webp_bytes(buffer: bytes):
+    reader = ByteReader(buffer, False)
+    # -- Webp file header --
+    reader.skip(4)
+    file_size = int.from_bytes(reader.read(4))
+    reader.skip(4)
+    file_format: Literal["VP8 ", "VP8L"] = reader.read(4, True).decode()
+    print(file_format)
+
 img_bytes = create_webp_pyfile("images/python.png", "out.py")
 from out import buffer
-
-print(buffer)
+decode_webp_bytes(buffer)
